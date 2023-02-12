1124cb98d544dff092cbbcd7aba9f18a3b63c7b8
Maksym Shemet <semetm25@gmail.com>
2023-02-12 18:00:06 +0100

    init

--- terraform/aws/compute.tf
+++ terraform/aws/compute.tf
@@ -69,3 +69,8 @@ output "private_key" {
   value     = tls_private_key.wp_priv_key.private_key_pem
   sensitive = true
 }
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
--- terraform/aws/startup.sh
+++ terraform/aws/startup.sh
@@ -5,6 +5,53 @@ apt update
 apt install ansible -y
 
 mkdir /tmp/ansible
+mkdir /tmp/scripts
+
+cat <<EOT >> /tmp/scripts/download_WP.sh
+#!/bin/bash
+
+while [ "\$#" -gt 0 ]; do
+  case "\$1" in
+    -v|--version)
+      wp_version="\$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown argument: \$1"
+      exit 1
+      ;;
+  esac
+done
+
+# Define the target directory
+TARGET_DIR="/var/www/html"
+if [ ! -d "\$TARGET_DIR" ]; then
+  mkdir "\$TARGET_DIR"
+fi
+
+
+# Define wordpress url
+WP_VERSION="wordpress-\$wp_version"
+WP_URL="https://wordpress.org/\$WP_VERSION.tar.gz"
+
+# Download the latest version of WordPress
+curl -L \$WP_URL -o \$WP_VERSION.tar.gz
+
+# Unpack the WordPress archive
+tar xzf \$WP_VERSION.tar.gz
+
+# Remove the archive
+rm \$WP_VERSION.tar.gz
+
+# Move the contents of the WordPress directory to the target directory
+if [ -d "\$TARGET_DIR" ]; then
+  rm -rf "\$TARGET_DIR"
+fi
+mv wordpress "\$TARGET_DIR"
+
+echo "WordPress has been successfully downloaded and unpacked into \$TARGET_DIR"
+
+EOT
 
 cat <<EOT >> /tmp/ansible/lamp.yaml
 ---
@@ -49,7 +96,8 @@ cat <<EOT >> /tmp/ansible/lamp.yaml
         name: mysql
         state: started
         enabled: yes
-
+    - name: Download and mv wordpress
+      script: /tmp/scripts/download_WP.sh --version 6.1.1
 EOT
 
 cd /tmp/ansible
